08544f366a872daf37daa02d214f3d209df4883b
Genbu Hase <hasegenbu@gmail.com>
2019-04-28 19:38:19 +0900

    substitute実装

--- src/Equript.ts
+++ src/Equript.ts
@@ -1,9 +1,32 @@
 export namespace Equript {
 	export class Equation {
-		public args: String[];
+		public args: string[];
 
-		constructor (public formula: String) {
+		/** 公式を生成します */
+		constructor (public formula: string) {
 			this.args = formula.replace(/\s/g, "").match(/[a-zA-Z]/g).filter((arg, index, args) => args.indexOf(arg) === index);
+		}
+
+		/** 指定された値を代入した方程式を返します */
+		public substitute (args: { [name: string]: number } = {}) {
+			for (const arg of this.args) args[arg] = args[arg] || 0;
+	
+			let result = this.formula;
+	
+			for (const arg in args) {
+				const value = args[arg];
+	
+				result = result
+					.replace(new RegExp(`(?<=\\w)${arg}(?=\\w)`, "g"), `*${value}*`)
+					.replace(new RegExp(`(?<=\\w)${arg}`, "g"), `*${value}`)
+					.replace(new RegExp(`${arg}(?=\\w)`, "g"), `${value}*`)
+					.replace(new RegExp(arg, "g"), value + "");
+			}
+	
+			const reversePrefixes = (result.match(/(\+ ?\-|\- ?\-)/g) || []);
+			for (const prefix of reversePrefixes) result = result.replace(prefix, prefix.replace(/\s/g, "") == "+-" ? "- " : "+ ");
+	
+			return result;
 		}
 	}
 }
